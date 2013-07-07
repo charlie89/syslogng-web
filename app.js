6256c7698a11c46855f8ceb6c5a7299d3f393d90
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-07-06 21:05:08 -0400

    Use socket.io to stream log messages

--- app.js
+++ app.js
@@ -5,8 +5,9 @@
 var express = require('express'), 
 	routes = require('./routes'), 
 	http = require('http'), 
-	path = require('path'),
-	mongodb = require('mongodb');
+	path = require('path'), 
+	mongodb = require('mongodb'), 
+	sio = require('socket.io');
 
 var app = express();
 
@@ -26,9 +27,45 @@ if ('development' == app.get('env')) {
 	app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/api/messages', routes.messages);
+var server = http.createServer(app);
+var io = sio.listen(server);
 
-http.createServer(app).listen(app.get('port'), function() {
+io.sockets.on('connection', function(socket) {
+
+	var logsCollection = null;
+
+	mongodb.MongoClient.connect('mongodb://db.trd:27017/syslog', function(err, db) {
+
+		if (err)
+			throw err;
+
+		logsCollection = db.collection('messages');
+
+		var stream = logsCollection.find({}, {
+			tailable : true,
+			await_data : true,
+			numberOfRetries : -1,
+			fields : {
+				'PROGRAM' : 1,
+				'PRIORITY' : 1,
+				'MESSAGE' : 1,
+				'DATE' : 1
+			},
+			sort : {
+				'$natural' : -1
+			}			
+		})
+		.stream()
+		.on('data', function(data) {
+			if (data) {
+				socket.emit('message', data);
+			}
+		});
+	});
+});
+
+app.get('/', routes.index);
+
+server.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -3,43 +3,6 @@
  * GET home page.
  */
 
-var MongoClient = require('mongodb').MongoClient;
-
-var logsCollection = null;
-
-MongoClient.connect('mongodb://db.trd:27017/syslog', function (err, db) {
-	if (err) throw err;
-	
-	logsCollection = db.collection('messages');
-	
-});
-
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
-};
-
-// GET /api/messages
-exports.messages = function (req, res) {
-	logsCollection.find({}, {
-		fields: {
-			'PROGRAM': 1,
-			'PRIORITY': 1,
-			'MESSAGE': 1,
-			'DATE': 1
-		},
-		sort: {
-			'DATE': -1
-		}
-	}).toArray(function (err, results) {
-		if (err) {
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		else {
-			res.send({
-				messages: results
-			});
-		}
-	});
-};
+}; 
