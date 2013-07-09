d91273992fd1eb1b89bd65660fcd54859e678c99
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-07-09 18:21:51 -0400

    Branding

--- app.js
+++ app.js
@@ -69,5 +69,5 @@ io.sockets.on('connection', function(socket) {
 
 // start the server
 server.listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + app.get('port'));
+	console.log('syslogng-web listening on port ' + app.get('port'));
 });
