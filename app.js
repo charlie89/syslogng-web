07a13430589165f73bdfc475d2afb6687e9e1277
Nicolas Laplante <nicolas.laplante@gmail.com>
2014-01-03 18:06:07 -0500

    Remove bodyParser and methodOverride middlewares

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
